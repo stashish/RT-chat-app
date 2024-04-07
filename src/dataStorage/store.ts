39c9e1004b3effd0b7a4f46f2905f4bbb0189363
webManiac <wankhade.ashish@yahoo.com>
2024-04-08 01:12:46 +0530

    chat store & in memory added

---
+++ src/dataStorage/store.ts
@@ -0,0 +1,21 @@
+export class Store {
+    constructor () {
+
+    }
+
+    initRoom() {
+
+    }
+
+    getChats(room: string, limit: number, offset: number) {
+
+    }
+
+    addChats(room: string, limit: number, offset: number) {
+
+    }
+
+    upVoteMessage(room: string, chatId: string) {
+
+    }
+}
